7b31536009b3a79ce1a08bdfd1863bdef7592d20
shadsvm <91400290+shadsvm@users.noreply.github.com>
2024-08-12 09:05:09 +0200

    🔍️ improve seo.

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -6,6 +6,8 @@ export default createHandler(() => (
     document={({ assets, children, scripts }) => (
       <html lang="en">
         <head>
+          <title>SK - Resume </title>
+          <meta name="description" content="Samuel Kaminski, Full Stack Developer" />
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
